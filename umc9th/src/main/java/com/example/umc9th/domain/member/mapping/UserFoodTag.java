da7bf2a3297de39cf290821607aba6fb1123d378
DESKTOP-TROTBC9\samso <samsockbee@gmail.com>
2025-10-11 00:28:32 +0900

    feat: UserFoodTag Mapping

--- umc9th/src/main/java/com/example/umc9th/domain/member/mapping/UserFoodTag.java
+++ umc9th/src/main/java/com/example/umc9th/domain/member/mapping/UserFoodTag.java
@@ -1,4 +1,29 @@
 package com.example.umc9th.domain.member.mapping;
 
-public class UserFoodTag {
+import com.example.umc9th.domain.member.entity.Member;
+import com.example.umc9th.domain.store.entity.FoodTag;
+import com.example.umc9th.global.BaseEntity;
+import lombok.*;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user_food_tag",
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id","tag_id"})})
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class UserFoodTag extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // owning side
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Member member;
+
+    // owning side
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tag_id")
+    private FoodTag tag;
 }
