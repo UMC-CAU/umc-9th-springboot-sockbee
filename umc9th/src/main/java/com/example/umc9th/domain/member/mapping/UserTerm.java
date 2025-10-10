29956126ce975ccdbeefc00f28a8954ccf3e1254
DESKTOP-TROTBC9\samso <samsockbee@gmail.com>
2025-10-11 00:31:35 +0900

    feat: User Term Mapping

--- umc9th/src/main/java/com/example/umc9th/domain/member/mapping/UserTerm.java
+++ umc9th/src/main/java/com/example/umc9th/domain/member/mapping/UserTerm.java
@@ -1,4 +1,31 @@
 package com.example.umc9th.domain.member.mapping;
 
-public class UserTerm {
+import com.example.umc9th.domain.term.entity.Term;
+import com.example.umc9th.domain.member.entity.Member;
+import com.example.umc9th.global.BaseEntity;
+import lombok.*;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user_term",
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id","term_id"})})
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class UserTerm extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "term_id")
+    private Term term;
+
+    private Boolean agreed;
+    private LocalDateTime agreeDate;
 }
