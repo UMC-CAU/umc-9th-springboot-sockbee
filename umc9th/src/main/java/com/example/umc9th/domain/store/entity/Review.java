5fd2f308ce800787978c871ca72902f537d13325
DESKTOP-TROTBC9\samso <samsockbee@gmail.com>
2025-10-10 18:49:37 +0900

    feat: Review Entity 생성

--- umc9th/src/main/java/com/example/umc9th/domain/store/entity/Review.java
+++ umc9th/src/main/java/com/example/umc9th/domain/store/entity/Review.java
@@ -1,4 +1,33 @@
 package com.example.umc9th.domain.store.entity;
 
-public class Review {
+import com.example.umc9th.global.BaseEntity;
+import com.example.umc9th.domain.member.entity.Member;
+import lombok.*;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "review",
+        indexes = {@Index(columnList = "store_id"), @Index(columnList = "user_id")})
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class Review extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reviewId;
+
+    @Lob
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @Column(precision = 3, scale = 2)
+    private BigDecimal star;
 }
