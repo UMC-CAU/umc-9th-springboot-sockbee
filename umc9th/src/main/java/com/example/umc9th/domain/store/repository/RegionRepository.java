0a1db9ff1c584846677b46be0a9020443b60bf6e
DESKTOP-TROTBC9\samso <samsockbee@gmail.com>
2025-10-11 01:19:05 +0900

    Refactor: Region 및 Store Repository 패키지 위치 수정

--- umc9th/src/main/java/com/example/umc9th/domain/store/repository/RegionRepository.java
+++ umc9th/src/main/java/com/example/umc9th/domain/store/repository/RegionRepository.java
@@ -1,4 +1,4 @@
-package com.example.umc9th.domain.mission.repository;
+package com.example.umc9th.domain.store.repository;
 
 import com.example.umc9th.domain.store.entity.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
