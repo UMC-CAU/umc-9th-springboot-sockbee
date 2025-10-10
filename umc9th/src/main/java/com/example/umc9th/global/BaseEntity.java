d30a581599801d6b8ddc68cf70fffeafb2f8d23b
DESKTOP-TROTBC9\samso <samsockbee@gmail.com>
2025-10-10 18:15:16 +0900

    feat: Base Entity 설계

--- umc9th/src/main/java/com/example/umc9th/global/BaseEntity.java
+++ umc9th/src/main/java/com/example/umc9th/global/BaseEntity.java
@@ -1,4 +1,23 @@
 package com.example.umc9th.global;
 
-public class BaseEntity {
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.MappedSuperclass;
+import javax.persistence.EntityListeners;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public abstract class BaseEntity {
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
