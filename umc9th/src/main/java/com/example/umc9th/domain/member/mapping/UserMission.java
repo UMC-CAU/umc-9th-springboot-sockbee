fa85b09f0699d367237ae01e441d55508122e026
DESKTOP-TROTBC9\samso <samsockbee@gmail.com>
2025-10-11 00:43:25 +0900

    feat: User Mission Mapping

--- umc9th/src/main/java/com/example/umc9th/domain/member/mapping/UserMission.java
+++ umc9th/src/main/java/com/example/umc9th/domain/member/mapping/UserMission.java
@@ -1,4 +1,39 @@
 package com.example.umc9th.domain.member.mapping;
 
-public class UserMission {
+import com.example.umc9th.domain.mission.entity.Mission;
+import com.example.umc9th.domain.member.entity.Member;
+import com.example.umc9th.global.BaseEntity;
+import lombok.*;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "user_mission",
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id","mission_id"})})
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class UserMission extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mission_id")
+    private Mission mission;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private Status status;
+
+    private LocalDate joinedAt;
+    private LocalDate completedAt;
+
+    public enum Status {
+        JOINED, COMPLETED, CANCELLED
+    }
 }
