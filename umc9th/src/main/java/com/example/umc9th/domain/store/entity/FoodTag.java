960d48d4f102e84139dd551641fe3d57d8bdfe6f
DESKTOP-TROTBC9\samso <samsockbee@gmail.com>
2025-10-10 18:29:32 +0900

    feat: FoodTag Entity 생성

--- umc9th/src/main/java/com/example/umc9th/domain/store/entity/FoodTag.java
+++ umc9th/src/main/java/com/example/umc9th/domain/store/entity/FoodTag.java
@@ -1,4 +1,27 @@
 package com.example.umc9th.domain.store.entity;
 
-public class FoodTag {
+import com.example.umc9th.global.BaseEntity;
+import lombok.*;
+
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "food_tag")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class FoodTag extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long tagId;
+
+    @Column(length = 50)
+    private String name;
+
+    @OneToMany(mappedBy = "tag", fetch = FetchType.LAZY)
+    private Set<com.example.umc9th.domain.store.entity.Store> stores = new HashSet<>();
+
+    @OneToMany(mappedBy = "tag", fetch = FetchType.LAZY)
+    private Set<com.example.umc9th.domain.member.mapping.UserFoodTag> userFoodTags = new HashSet<>();
 }
