11b65c5655297136a758c05051ba40e8f4cfba45
DESKTOP-TROTBC9\samso <samsockbee@gmail.com>
2025-10-11 01:05:32 +0900

    feat: UserRegionprogress Mapping

--- umc9th/src/main/java/com/example/umc9th/domain/member/mapping/UserRegionProgress.java
+++ umc9th/src/main/java/com/example/umc9th/domain/member/mapping/UserRegionProgress.java
@@ -1,4 +1,29 @@
 package com.example.umc9th.domain.member.mapping;
 
-public class UserRegionProgress {
+import com.example.umc9th.domain.member.entity.Member;
+import com.example.umc9th.domain.store.entity.Region;
+import com.example.umc9th.global.BaseEntity;
+import lombok.*;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user_region_progress",
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id","region_id"})})
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class UserRegionProgress extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+    private Integer pointCount;
 }
