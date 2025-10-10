0df9554d53b3ff27101d0352390e7feb0c57c039
DESKTOP-TROTBC9\samso <samsockbee@gmail.com>
2025-10-11 00:31:11 +0900

    feat: Term Entity 생성

--- umc9th/src/main/java/com/example/umc9th/domain/terms/entity/Term.java
+++ umc9th/src/main/java/com/example/umc9th/domain/terms/entity/Term.java
@@ -1,4 +1,30 @@
 package com.example.umc9th.domain.terms.entity;
 
-public class Term {
+import com.example.umc9th.global.BaseEntity;
+import lombok.*;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "term")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class Term extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private java.time.LocalDateTime applyDate;
+
+    @Lob
+    private String terms;
+
+    private Boolean essential;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    public enum Status {
+        ACTIVE, INACTIVE
+    }
 }
