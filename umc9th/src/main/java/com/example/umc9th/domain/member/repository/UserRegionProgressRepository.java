eb1c89d58464b9651f02ff34d4b8db5ae91334b8
DESKTOP-TROTBC9\samso <samsockbee@gmail.com>
2025-10-11 01:14:32 +0900

    feat: UserRegionProgress Repository 추가

---
+++ umc9th/src/main/java/com/example/umc9th/domain/member/repository/UserRegionProgressRepository.java
@@ -0,0 +1,7 @@
+package com.example.umc9th.domain.member.repository;
+
+import com.example.umc9th.domain.member.mapping.UserRegionProgress;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRegionProgressRepository extends JpaRepository<UserRegionProgress, Long> {
+}
