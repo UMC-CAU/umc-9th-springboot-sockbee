55c4c230fe45486cedf0b526052ab12a3ff79d01
DESKTOP-TROTBC9\samso <samsockbee@gmail.com>
2025-10-11 00:25:33 +0900

    feat: Mission  Entity 생성

--- umc9th/src/main/java/com/example/umc9th/domain/mission/entity/Mission.java
+++ umc9th/src/main/java/com/example/umc9th/domain/mission/entity/Mission.java
@@ -1,4 +1,31 @@
 package com.example.umc9th.domain.mission.entity;
 
-public class Mission {
+import com.example.umc9th.global.BaseEntity;
+import com.example.umc9th.domain.store.entity.Store;
+import lombok.*;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "mission")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class Mission extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long missionId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    private LocalDateTime dueDate;
+
+    private Integer rewardPoint;
+
+    @OneToMany(mappedBy = "mission", fetch = FetchType.LAZY)
+    private Set<com.example.umc9th.domain.member.mapping.UserMission> userMissions = new HashSet<>();
 }
