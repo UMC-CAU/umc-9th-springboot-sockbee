9d6ed60c52495175c171a20854449e60da80e6f9
DESKTOP-TROTBC9\samso <samsockbee@gmail.com>
2025-10-30 15:02:45 +0900

    feat : 리뷰 작성하는 페이지. 저장은 save() 활용

--- umc9th/src/main/java/com/example/umc9th/domain/review/repository/ReviewRepository.java
+++ umc9th/src/main/java/com/example/umc9th/domain/review/repository/ReviewRepository.java
@@ -3,5 +3,9 @@ package com.example.umc9th.domain.review.repository;
 import com.example.umc9th.domain.review.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    // 특정 매장의 리뷰 전체 조회
+    List<Review> findByStore_Id(Long storeId);
 }
