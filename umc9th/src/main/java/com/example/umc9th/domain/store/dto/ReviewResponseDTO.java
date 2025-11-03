2ec34f7280b68255b9c58e600fc2610077bfd1d3
DESKTOP-TROTBC9\samso <samsockbee@gmail.com>
2025-11-04 04:19:42 +0900

    feat : 커스텀 레파지토리 인터페이스 정의

---
+++ umc9th/src/main/java/com/example/umc9th/domain/store/dto/ReviewResponseDTO.java
@@ -0,0 +1,12 @@
+package com.example.umc9th.domain.store.dto;
+
+import java.time.LocalDateTime;
+
+public record ReviewResponseDTO(
+        Long reviewId,
+        Long storeId,
+        String storeName,
+        Double star,
+        String content,
+        LocalDateTime createdAt
+) {}
