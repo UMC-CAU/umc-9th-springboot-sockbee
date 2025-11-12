4bdacc79c5a9ced2a308bd3ef17513e0e60db90c
DESKTOP-TROTBC9\samso <samsockbee@gmail.com>
2025-11-13 02:35:05 +0900

    feat : 응답 DTO 생성

---
+++ umc9th/src/main/java/com/example/umc9th/domain/test/dto/res/TestResDTO.java
@@ -0,0 +1,10 @@
+package com.example.umc9th.domain.test.dto.res;
+
+public class TestResDTO {
+
+    @Builder
+    @Getter
+    public static class Testing {
+        private String testing;
+    }
+}
