00cc63c0f05276d6454f2518122c82de34382981
DESKTOP-TROTBC9\samso <samsockbee@gmail.com>
2025-11-20 14:28:26 +0900

    feat: 2. 가게에 리뷰 추가하기 기능 구현을 위한 기존 커스텀 레파지토리 삭제

--- umc9th/src/main/java/com/example/umc9th/domain/review/repository/ReviewRepository.java
+++ umc9th/src/main/java/com/example/umc9th/domain/review/repository/ReviewRepository.java
@@ -2,11 +2,6 @@ package com.example.umc9th.domain.review.repository;
 
 import com.example.umc9th.domain.review.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.umc9th.domain.review.custom.ReviewRepositoryCustom;
 
-import java.util.List;
-
-public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewRepositoryCustom {
-    // 특정 매장의 리뷰 전체 조회
-    List<Review> findByStoreId(Long storeId);
+public interface ReviewRepository extends JpaRepository<Review, Long> {
 }
