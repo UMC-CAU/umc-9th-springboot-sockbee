c7b6d143398ff1b0ff32cf119ad45bc9a2004885
DESKTOP-TROTBC9\samso <samsockbee@gmail.com>
2025-11-26 19:17:58 +0900

    feat: ReviewResponseDTO에 AllArgsConstructor 추가

--- umc9th/src/main/java/com/example/umc9th/domain/review/dto/ReviewResponseDTO.java
+++ umc9th/src/main/java/com/example/umc9th/domain/review/dto/ReviewResponseDTO.java
@@ -1,5 +1,6 @@
 package com.example.umc9th.domain.review.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,6 +9,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @Builder
+@AllArgsConstructor
 public class ReviewResponseDTO {
 
     private final Long reviewId;
